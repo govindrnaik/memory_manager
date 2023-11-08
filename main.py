eee65a4b8ec92d2bcd40c1a521e92898f5ed2851
govind ratnakar naik <govindrnaik.trk@gmail.com>
2023-11-08 22:39:40 +0530

    tasks

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from routes.tasks import router as tasks
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "tasks": tasks})
+
+
+
 
 app.include_router(router=tasks)
 if __name__ == "__main__":
