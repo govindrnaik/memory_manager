8964db01ba7f2d9a0d0d121226231aaf4b50c8a3
govind ratnakar naik <govindrnaik.trk@gmail.com>
2023-11-08 22:42:15 +0530

    working on users

--- models.py
+++ models.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
 
-
 class Task(BaseModel):
     title: str
     description: str
+    status: bool = False
+
+
+class User(BaseModel):
+    username: str
+    email: str
+    password: str
+    is_active: bool = True
+    is_superuser: bool = False
